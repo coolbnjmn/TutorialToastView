86afeacd25ebe416e0c7b30f2eed61301210a486
Benjamin Hendricks <ben@govyrl.io>
2015-11-07 20:16:47 -0800

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'TutorialToastView' do
+
+end
+
+target 'TutorialToastViewTests' do
+
+end
+
---
+++ TutorialToastView/TutorialToastView.h
@@ -0,0 +1,19 @@
+//
+//  TutorialToastView.h
+//  TutorialToastView
+//
+//  Created by Benjamin Hendricks on 11/7/15.
+//  Copyright © 2015 coolbnjmn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TutorialToastView.
+FOUNDATION_EXPORT double TutorialToastViewVersionNumber;
+
+//! Project version string for TutorialToastView.
+FOUNDATION_EXPORT const unsigned char TutorialToastViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TutorialToastView/PublicHeader.h>
+
+
